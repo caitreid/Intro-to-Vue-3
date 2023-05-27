930d8413cd8d83922381bfee5d113a79b29a254c
Caitlin Reid <ctlnrd@gmail.com>
2023-05-27 16:59:30 -0400

    lesson 10 done

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            cart: [],
+            premium: true,
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
+            // console.log(id)
+            console.log('main js update cart')
         },
-        updateVariant(index) {
-            this.selectedVariant = index
+        removeFromCart(id) {
+            // this.cart.pop(id)
+            // console.log(id)
+            console.log('remove')
+            console.log('cart', this.cart)
+
+            let index = this.cart.indexOf(id)
+
+            if (index !== -1 ){
+
+                this.cart.splice(index, 1)
+            }
         }
     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-    }
+
 })
